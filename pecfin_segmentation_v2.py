bd1f290622aa8557cacc322cf190f311e766d9eb
Nicholas Lammers <nlammers@berkeley.edu>
2023-02-09 17:44:22 -0800

    Progress with pec fin segmentation code

--- pecfin_segmentation_v2.py
+++ pecfin_segmentation_v2.py
@@ -17,7 +17,7 @@ import pickle
 import os.path
 import os
 import math
-
+import dash_daq as daq
 
 def segment_pec_fins(dataRoot, nn_k=10):
 
@@ -38,18 +38,19 @@ def segment_pec_fins(dataRoot, nn_k=10):
         raise Exception("Selected dataset has no nucleus data. Have you run extract_nucleus_stats?")
 
     # look for saved data from previous curation effort
-    selected_points_prev = []
+    not_fin_points_prev = []
+    fin_points_prev = []
     vl_prev = []
 
     if os.path.isfile(propPath):
         df = pd.read_csv(propPath)
 
         # load key info from previous session
-        sc_path = curationPath + 'selected_points.pkl'
+        sc_path = curationPath + 'not_fin_points.pkl'
         if os.path.isfile(sc_path):
             with open(sc_path, 'rb') as fn:
-                selected_points_prev = pickle.load(fn)
-            selected_points_prev = json.loads(selected_points_prev)
+                not_fin_points_prev = pickle.load(fn)
+            not_fin_points_prev = json.loads(not_fin_points_prev)
 
             vl_path = curationPath + 'value_slider.pkl'
             with open(vl_path, 'rb') as fn:
@@ -108,7 +109,6 @@ def segment_pec_fins(dataRoot, nn_k=10):
         return fig
     f = create_figure()
 
-
     app.layout = html.Div([#html.Button('Delete', id='delete'),
                         html.Button('Clear Selection', id='clear'),
                         html.Button('Save', id='save-button'),
@@ -116,50 +116,87 @@ def segment_pec_fins(dataRoot, nn_k=10):
                         html.P(id='save-button-hidden', style={'display': 'none'}),
                         dcc.Graph(id='3d_scat', figure=f),
                         #html.Div('selected:'),
-                        html.Div(id='selected_points', hidden=True),
+                        html.Div(id='not_fin_points', hidden=True),
+                        html.Div(id='fin_points', hidden=True),
                         html.Div(id='pfin_nuclei', hidden=True),
                         dcc.RangeSlider(-round(10*nn_threshold), round(10*nn_threshold), value=val_defaults, id='select-slider'),
-                        html.Div(id='output-container-range-slider')
-    ])
+                        html.Div(id='output-container-range-slider'),
+                        html.Div(
+                            daq.ToggleSwitch(
+                                id='class-toggle-switch',
+                                value=False
+                            )),
+                        html.Div(id='my-toggle-switch-output')]
+                        )
 
+    @app.callback(
+        Output('my-toggle-switch-output', 'children'),
+        Input('class-toggle-switch', 'value'))
+    def update_output(value):
+        value = json.dumps(value)
+        return value
 
-    @app.callback(Output('selected_points', 'children'),
+    @app.callback([Output('not_fin_points', 'children'),
+                   Output('fin_points', 'children')],
                     [Input('3d_scat', 'clickData'),
-                    Input('clear', 'n_clicks')],
-                    [State('selected_points', 'children')])
+                     Input('clear', 'n_clicks'),
+                     Input('my-toggle-switch-output', 'children')],
+                    [State('not_fin_points', 'children'),
+                     State('fin_points', 'children')])
 
-    def select_point(clickData, n_clicks, selected_points):
+    def select_point(clickData, n_clicks, toggle_switch, not_fin_points, fin_points):
         ctx = dash.callback_context
         ids = [c['prop_id'] for c in ctx.triggered]
         changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-
-        if selected_points:
-            results = json.loads(selected_points)
+        toggle_val = toggle_switch == "false"
+        #print(toggle_val)
+        # check for previous selections
+        if not_fin_points:
+            not_fin_results = json.loads(not_fin_points)
         else:
-            results = []
+            not_fin_results = []
 
-        # print('inside sselect points')
-        # print(selected_points_prev)
-        if len(selected_points_prev) > 0:
-            for p in selected_points_prev:
-                if p not in results:
-                    results.append(p)
+        if fin_points:
+            fin_results = json.loads(fin_points)
+        else:
+            fin_results = []
+
+        # check for saved points
+        if len(not_fin_points_prev) > 0:
+            for p in not_fin_points_prev:
+                if p not in not_fin_results:
+                    not_fin_results.append(p)
+
+        if len(fin_points_prev) > 0:
+            for p in fin_points_prev:
+                if p not in fin_results:
+                    fin_results.append(p)
 
         if '3d_scat.clickData' in ids:
-            if True:#clickData:
+            if not toggle_val:
                 for p in clickData['points']:
-                    if p not in results:
-                        results.append(p)
+                    if p not in not_fin_results:
+                        not_fin_results.append(p)
                     else:
                         # if selected point is in list, then lets unselect
-                        results.remove(p)
+                        not_fin_results.remove(p)
+            else:
+                for p in clickData['points']:
+                    if p not in not_fin_results:
+                        fin_results.append(p)
+                    else:
+                        # if selected point is in list, then lets unselect
+                        fin_results.remove(p)
+
         if 'clear' in changed_id:
-           results = []
+            not_fin_results = []
+            fin_results = []
 
-        results = json.dumps(results)
-        #coordinates = [[p['x']] for p in results]
+        not_fin_results = json.dumps(not_fin_results)
+        fin_results = json.dumps(fin_results)
+        #coordinates = [[p['x']] for p in not_fin_results]
 
-        return results
+        return not_fin_results, fin_results
 
     @app.callback(
         Output('output-container-range-slider', 'children'),
@@ -170,26 +207,27 @@ def segment_pec_fins(dataRoot, nn_k=10):
 
     @app.callback([Output('3d_scat', 'figure'),
                    Output('pfin_nuclei', 'children')],
-                [Input('selected_points', 'children'),
-                 Input('output-container-range-slider', 'children'),
-                 Input('calc-button', 'n_clicks'),
-                 Input('select-slider', 'value')])
+                [Input('not_fin_points', 'children'),
+                 Input('fin_points', 'children'),
+                 Input('output-container-range-slider', 'children')])
 
-    def chart_3d(selected_points, select_range, n_clicks, slider_range):
+    def chart_3d(not_fin_points, fin_points, select_range):
         global f
         # check to see which values have changed
         changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
 
         f = create_figure()
         f.update_layout(uirevision="Don't change")
-        selected_points = json.loads(selected_points) if selected_points else []
-        if selected_points:
+        not_fin_points = json.loads(not_fin_points) if not_fin_points else []
+        fin_points = json.loads(fin_points) if fin_points else []
+
+        if not_fin_points:
             f.add_trace(
                 go.Scatter3d(
                     mode='markers',
-                    x=[p['x'] for p in selected_points],
-                    y=[p['y'] for p in selected_points],
-                    z=[p['z'] for p in selected_points],
+                    x=[p['x'] for p in not_fin_points],
+                    y=[p['y'] for p in not_fin_points],
+                    z=[p['z'] for p in not_fin_points],
                     marker=dict(
                         color='crimson',
                         size=7,
@@ -202,11 +240,30 @@ def segment_pec_fins(dataRoot, nn_k=10):
                 )
             )
 
+        if fin_points:
+            f.add_trace(
+                go.Scatter3d(
+                    mode='markers',
+                    x=[p['x'] for p in fin_points],
+                    y=[p['y'] for p in fin_points],
+                    z=[p['z'] for p in fin_points],
+                    marker=dict(
+                        color='lightgreen',
+                        size=7,
+                        line=dict(
+                            color='black',
+                            width=4
+                        )
+                    ),
+                    showlegend=False
+                )
+            )
         pec_fin_nuclei = []
-        if selected_points and (('select-slider' in changed_id) | ('calc-button' in changed_id)):
+        print(fin_points)
+        if not_fin_points and (('select-slider' in changed_id) | ('calc-button' in changed_id)):
 
-            #oint_dict = json.loads(selected_points)
-            xyz = np.reshape([[p['x'], p['y'], p['z']] for p in selected_points], (len(selected_points), 3))
+            #oint_dict = json.loads(not_fin_points)
+            xyz = np.reshape([[p['x'], p['y'], p['z']] for p in not_fin_points], (len(not_fin_points), 3))
             # Fit a 3rd order, 2d polynomial
             m = polyfit2d(xyz[:, 0], xyz[:, 1], xyz[:, 2], order=2)
             # Evaluate it on a grid...
@@ -255,10 +312,10 @@ def segment_pec_fins(dataRoot, nn_k=10):
         Output('save-button-hidden', 'children'),
         [Input('save-button', 'n_clicks'),
             Input('pfin_nuclei', 'children'),
-            Input('selected_points', 'children'),
+            Input('not_fin_points', 'children'),
             Input('select-slider', 'value')])
 
-    def clicks(n_clicks, pec_fin_nuclei, selected_points,slider_values):
+    def clicks(n_clicks, pec_fin_nuclei, not_fin_points,slider_values):
         changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
         if 'save-button' in changed_id:
             lbl_vec = []
@@ -272,9 +329,9 @@ def segment_pec_fins(dataRoot, nn_k=10):
             write_file = dataRoot + filename + '_nucleus_props.csv'
             df.to_csv(write_file)
 
-            write_file2 = dataRoot + filename + '_curation_info/selected_points.pkl'
+            write_file2 = dataRoot + filename + '_curation_info/not_fin_points.pkl'
             with open(write_file2, 'wb') as wf:
-                pickle.dump(selected_points, wf)
+                pickle.dump(not_fin_points, wf)
 
             write_file3 = dataRoot + filename + '_curation_info/value_slider.pkl'
             with open(write_file3, 'wb') as wf:
